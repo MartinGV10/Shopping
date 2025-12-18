e34025376f6183f3375db6a9acbf60721a6492cd
MartinGV10 <mganen10@gmail.com>
2025-12-18 13:55:25 -0500

    Refactored API fetching to a specific component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,10 @@
-import useCategory from "./useCategory"
 import { Link } from "react-router"
 import apple from '/apple.jpg'
 import clohtes from '/clothes.jpg'
 import watch from '/watch.jpg'
-// import useImages from "./useImages"
+import UseImages from "./useImages"
 
 function Home() {
-    const {products: tablets} = useCategory('smartphones', 5, 0)
-    const {products: watches} = useCategory('mens-watches', 5, 0)
-    // const {products: watches, error, load} = useCategory('watches', 8, 0)
-    // const {products: tablets, error, load} = useCategory('tablets', 8, 0)
-
-    // if (load) return <p>Loading...</p>
-    // if (error) return <p>A network error was encountered</p>
-
     return (
         <>
             <div className="home">
@@ -59,7 +50,7 @@ function Home() {
                         <p>The biggest trends, all in one place</p>
                     </div>
                     <div className="h-items">
-                        {tablets.map((p) => (
+                        {/* {tablets.map((p) => (
                             <div className="item" key={p.id}>
                                 <div className="item-top">
                                     <img src={p.thumbnail} className="item-img"/>
@@ -79,8 +70,8 @@ function Home() {
                                     <p className="link"><Link to='/shop'>Buy Now</Link></p>
                                 </div>
                             </div>
-                        ))}
-                        {/* <useImages catName={smartphones}/> */}
+                        ))} */}
+                        <UseImages category={'smartphones'} limit={5} skip={0}/>
                     </div>
                 </div>
 
@@ -90,7 +81,7 @@ function Home() {
                         <p>Get them now before they're gone</p>
                     </div>
                     <div className="h-items">
-                        {watches.map((p) => (
+                        {/* {watches.map((p) => (
                             <div className="item" key={p.id}>
                                 <div className="item-top">
                                     <img src={p.thumbnail} className="item-img"/>
@@ -103,20 +94,15 @@ function Home() {
                                 </div>
                                 
                                 <div className="item-bot">
-                                    {/* <p className="ship">{p.shippingInformation}</p> */}
-                                    {/* <p>{p.stock} {p.availabilityStatus}</p>
-                                    <p className="rating">{p.rating}/5 
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="lightgray" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                                        </svg>
-                                    </p> */}
+
                                 </div>
 
                                 <div className="item-overlay">
                                     <p className="link"><Link to='/shop'>Buy Now</Link></p>
                                 </div>
                             </div>
-                        ))}
+                        ))} */}
+                        <UseImages category={'mens-watches'} limit={5} skip={0}/>
                     </div>
                 </div>
 
--- src/useImages.jsx
+++ src/useImages.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router"
 import useCategory from "./useCategory"
 
-const useImages = ({ catName }) => {
-    const {products: catName} = useCategory(catName, 5, 0)
+const UseImages = ({ category, limit, skip }) => {
+    const { products, load, error } = useCategory(category, limit, skip)
 
+    if (load) return <p>Loading...</p>
+    if (error) return <p>A network error was encountered</p>
     return (
         <>
-        {catName.map((p) => (
+        {products.map((p) => (
+            <Link to='/shop'> 
             <div className="item" key={p.id}>
                 <div className="item-top">
                     <img src={p.thumbnail} className="item-img"/>
@@ -21,9 +25,9 @@ const useImages = ({ catName }) => {
                     {/* <p className="ship">{p.shippingInformation}</p> */}
                     {/* <p>{p.stock} {p.availabilityStatus}</p>
                     <p className="rating">{p.rating}/5 
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="lightgray" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                        </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="lightgray" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                    </svg>
                     </p> */}
                 </div>
 
@@ -31,9 +35,10 @@ const useImages = ({ catName }) => {
                     <p className="link"><Link to='/shop'>Buy Now</Link></p>
                 </div>
             </div>
+            </Link>
         ))}
         </>
     )
 }
 
-export default useImages
+export default UseImages
